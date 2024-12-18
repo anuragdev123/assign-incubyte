20263a7ad5e5edafa4385e4c60090ddcabed7613
anuragdev123 <anuragpurwar007@gmail.com>
2024-12-18 19:21:37 +0530

    imported main file and postive number for test

---
+++ calc.test.js
@@ -0,0 +1,10 @@
+const add = require('./calc');
+
+
+const tests = [
+    { input: "", expected: 0 },
+    { input: "1", expected: 1 },
+    { input: "1,5", expected: 6 },
+    { input: "1\n2,3", expected: 6 },
+    { input: "2,3,4", expected: 9 },
+];
